4b3e5c125c1d692d2375c9e83ee9f76b9d38a072
gks6141 <83192719+gks6141@users.noreply.github.com>
2022-05-04 19:57:10 +0900

    Add files via upload

---
+++ 수박수박수.py
@@ -0,0 +1,13 @@
+def water_melon(n):
+    s = "수박" * n
+    return s[:n]
+
+
+# 실행을 위한 테스트코드입니다.
+print("n이 3인 경우: " + water_melon(3));
+print("n이 4인 경우: " + water_melon(4));
+
+
+
+def solution(n):
+    return "".join(["수박"[i%2] for i in range(n)])
